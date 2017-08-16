9fa63b1f17df4529c05f07802b38a4af913d4d84
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-16 17:01:55 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
-class Dog
+class Dog < ActiveRecord::Base
+
+  
+  # attr_accessor :name, :breed
+  # def initialize(name:, breed:)
+  #   @name = name
+  #   @breed = breed
+  # end
+  #
+  # def self.save
+  #
+  # end
+  #
+  # def self.create(hash)
+  #   Dog.create(hash)
+  # end
+
 end
